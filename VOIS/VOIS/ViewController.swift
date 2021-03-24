1214d7d32fe96392f431baea900ec7dc049d5953
Mahmoud Elkarargy <mahmoud.elkarargy@gmail.com>
2021-03-25 00:04:12 +0200

    init

---
+++ VOIS/VOIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VOIS
+//
+//  Created by Mahmoud Elkarargy on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
